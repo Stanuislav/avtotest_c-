996431ff66fd9bb31c10431a19438ea558357696
stasshur3@yandex.ru <stasshur3@yandex.ru>
2025-08-12 22:58:51 +0300

    HomeWork_1.4

---
+++ addreesbook_avtotest/addreesbook_avtotest/ContactData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdressbokkTests
+{
+    internal class ContactData
+    {
+        private string firstname;
+        private string secondname = "";
+        private string lastname = "";
+
+        public ContactData (string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+
+        public string Firstname { get { return firstname; }  set { firstname = value; } }
+        public string Secondname { get { return secondname; } set { secondname = value; } }
+        public string Lastname { get { return lastname; } set { lastname = value; } }
+
+
+    }
+}
